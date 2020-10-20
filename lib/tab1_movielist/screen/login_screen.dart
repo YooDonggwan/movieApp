b82059bd2265e6f3cd8c709feaa604578565fb33
YooDonggwan <dbehdrhks@naver.com>
2020-10-20 21:06:57 +0900

    change login pg UI && need to connect firebase

--- lib/tab1_movielist/screen/login_screen.dart
+++ lib/tab1_movielist/screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:movieApp/main.dart';
+import 'package:movieApp/style/theme.dart' as Style;
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -10,8 +11,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
 
   final users = {
-    'dbehdrhks@naver.com' : 1234,
-    'ehdrhks@naver.com' : 12345,
+    'dbehdrhks@naver.com' : '1234',
+    'ehdrhks@naver.com' : '12345',
   };
 
   Duration get loginTime => Duration(milliseconds: 2250);
@@ -43,9 +44,28 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'Moviary',
-      logo: 'assets/images/ecorp-lightblue.png',
       onLogin: _authUser,
       onSignup: _authUser,
+      theme: LoginTheme(
+        primaryColor: Style.Colors.mainColor,
+        titleStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      messages: LoginMessages(
+        usernameHint: '아이디',
+        passwordHint: '비밀번호',
+        confirmPasswordHint: '비밀번호 재확인',
+        loginButton: '로그인',
+        signupButton: '회원가입',
+        forgotPasswordButton: '비밀번호를 잊으셨나요?',
+        goBackButton: '돌아가기',
+        confirmPasswordError: '비밀번호가 다릅니다!',
+        recoverPasswordButton: '비밀번호 변경하기',
+        recoverPasswordDescription: '이메일로 임시 비밀번호를 전송하겠습니다',
+        recoverPasswordIntro: '비밀번호를 재설정해주세요',
+        recoverPasswordSuccess: '이메일로 비밀번호를 전송했습니다.'
+      ),
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) => MovieApp(),
